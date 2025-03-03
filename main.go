00fa16c457428bb8c293c36e194a9775788cb7ed
t57r <parshenkotaras@gmail.com>
2025-03-03 15:39:13 +0200

    Basic REPL

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func clearInput(text string) []string {
-	tokens := strings.Fields(text)
-	for idx := range tokens {
-		tokens[idx] = strings.ToLower(tokens[idx])
-	}
-	return tokens
-}
-
 func main() {
-	fmt.Println("Hello, World!")
+	startRepl()
 }
---
+++ repl.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func clearInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := clearInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+
+}
